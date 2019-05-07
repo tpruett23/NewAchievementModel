b8039879c93a89f07301056b3a9c989ef2752274
tpruett23 <vdpruett1@catamount.wcu.edu>
2019-05-07 14:32:07 -0400

    Update TimeCheck.java

--- app/src/main/java/achievements/TimeCheck.java
+++ app/src/main/java/achievements/TimeCheck.java
@@ -1,19 +1,25 @@
 package achievements;
 
 import android.content.Intent;
-
+/**
+ * The class checks to see if the time achievement has been met.
+ * @author Tori Pruett
+ * @version 1.0
+ */
 public class TimeCheck implements CheckAchievements {
     UserInfo UI = new UserInfo();
-    //AchievementFactory af = new AchievementFactory();
 
+    /**
+     * The method checks to see if the time achievement has been completed.
+     * @param ach The achievement to check the values against.
+     * @return true if completed, false if not.
+     */
     @Override
     public boolean checkAchievement(Achievements ach) {
         boolean check = false;
         if (UI.getTimePlayed() > ach.getDescriptorA().getTime()) {
 
                 check= true;
-
-               //AchievementFactory.achievements.add(ach);
 
         }
         return check;
