57f1898db8002f821ab8ced51e5820ab61cd551e
tpruett23 <vdpruett1@catamount.wcu.edu>
2019-05-07 15:00:33 -0400

    Update NotificationActivity.java

--- app/src/main/java/screens/NotificationActivity.java
+++ app/src/main/java/screens/NotificationActivity.java
@@ -6,11 +6,18 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
+/**
+ * Activity to send a notification.
+ * @author Tori Pruett
+ */
 public class NotificationActivity extends Activity {
-
+        /**
+         * Notification ID.
+         */
         public static final String NOTIFICATION_ID = "NOTIFICATION_ID";
-
+        /**
+         * This is called to create the activity before it is started.
+         */
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -18,7 +25,12 @@ public class NotificationActivity extends Activity {
             manager.cancel(getIntent().getIntExtra(NOTIFICATION_ID, -1));
             finish(); // since finish() is called in onCreate(), onDestroy() will be called immediately
         }
-
+        /**
+         * Gets the pending intent and sends the notification by using an intent.
+         * @param notificationId The id of the notification.
+         * @param context The context to create the intent.
+         * @return Returns the dismiss intent.
+         */
         public static PendingIntent getDismissIntent(int notificationId, Context context) {
             Intent intent = new Intent(context, NotificationActivity.class);
             //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
