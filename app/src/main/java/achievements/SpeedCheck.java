05fdc7bcae70ce45fbae1f95135b073aeb7df174
tpruett23 <vdpruett1@catamount.wcu.edu>
2019-05-07 14:21:30 -0400

    Update SpeedCheck.java

--- app/src/main/java/achievements/SpeedCheck.java
+++ app/src/main/java/achievements/SpeedCheck.java
@@ -1,10 +1,18 @@
 package achievements;
 
 import android.content.Intent;
-
+/**
+ * Checks if the speed achievement has been completed.
+ * @author Tori Pruett
+ * @version 1.0
+ */
 public class SpeedCheck implements CheckAchievements {
     UserInfo UI = new UserInfo();
-
+/**
+ * Checks to see if the achievement has been completed.
+ * @param ach The achievement instance to check the values against.
+ * @return true if completed, false if not.
+ */
     @Override
     public boolean checkAchievement(Achievements ach) {
         boolean check = false;
